076ab6052e7b8bf3443a8d3b4ce9418ae99a8629
Riku Hrarasawa <harasawa@imsnet.co.jp>
2026-05-29 14:51:20 +0900

    EditCategory.cshtml.csを修正

--- KakeiboApp/Pages/EditCategory.cshtml.cs
+++ KakeiboApp/Pages/EditCategory.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KakeiboApp.Models;
 
 namespace KakeiboApp.Pages
 {
     public class EditCategoryModel : PageModel
     {
-        public void OnGet()
+        private readonly KakeiboDbContext _db;
+
+        public EditCategoryModel(KakeiboDbContext db)
         {
+            _db = db;
+        }
+
+        public Category? Category { get; set; }
+
+        public async Task OnGetAsync(string id)
+        {
+            Category = await _db.Categories.FindAsync(id);
         }
     }
 }
