52885327f802862cb98cae06bfc6f9aed819b432
Riku Hrarasawa <harasawa@imsnet.co.jp>
2026-06-01 17:54:35 +0900

    設定画面CSS込み完全版

---
+++ KakeiboApp/Pages/EditCategory.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KakeiboApp.Models;
+
+namespace KakeiboApp.Pages
+{
+    public class EditCategoryModel : PageModel
+    {
+        private readonly KakeiboDbContext _db;
+
+        public EditCategoryModel(KakeiboDbContext db)
+        {
+            _db = db;
+        }
+
+        public Category? Category { get; set; }
+
+        public async Task OnGetAsync(string id)
+        {
+            Category = await _db.Categories.FindAsync(id);
+        }
+    }
+}
